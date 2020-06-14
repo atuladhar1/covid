de7872b2fb015e6e58370d8fc43d49305292ad8e
atuladhar1 <atuladhar1@gmail.com>
2020-06-14 17:01:05 -0500

    State properly stores the data, fixed a small bug with the set State

--- src/App.js
+++ src/App.js
@@ -7,32 +7,54 @@ class App extends React.Component{
 
   constructor(){
     super()
-    this.state={initialized: false, countryData:{}}
+    this.state={initialized: false, countryData:[]}
   }
 
-// API call from source and save it in the state.
+// API call from source and save it in the state as an array for easies data sorting.
   componentDidMount() {
     fetch("https://api.covid19api.com/summary")
     .then(response => response.json())
     .then(data=> {
+
+      let final=[[
+        "Countries",
+        "Confirmed",
+        "Deaths",
+        "Recovered"]]
+
       const Countries = data.Countries
-
       for (let country in Countries)
-        console.log(Countries[country].Country)
-        //TODO add the data to table
-        //TODO pass the table to map and table
-        //TODO Set function to properly sort the data and show the data accordingly
-
+          final.push(
+            [Countries[country].Country, 
+            Countries[country].TotalConfirmed, 
+            Countries[country].TotalDeaths,
+            Countries[country].TotalRecovered])
+      this.setState({initialized: true, countryData: final})
     })
   }
 
+/*TODOs:
+    Map the heatmap to countries
+    Create a table do display the data
+*/
+
 // Every function is called here. Map-container created as a container for the map to be displayed. Everything else is just calls.
   render(){
-    return (
-      <div>
-        <Table/>
-      </div>
-    );}
+    if (this.state.initialized)
+    {
+      console.log(this.state)
+      return (
+        <div>
+          <Table/>
+          <div id = "map-container" style = {{height: 200, width : 200}}></div>
+          <Map/>
+        </div>
+      )
+    }
+    else {
+      return <h1>wut</h1>
+    }
+    }
 }
 
 export default App;
