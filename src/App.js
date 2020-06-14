b44f952fd88129ad6e47beab72745f2b8b30ed78
atuladhar1 <atuladhar1@gmail.com>
2020-06-14 12:42:04 -0500

    Can properly store data in Arrays to pass to different functions

--- src/App.js
+++ src/App.js
@@ -7,22 +7,29 @@ class App extends React.Component{
 
   constructor(){
     super()
-    this.state={initialized: false, data:{}}
+    this.state={initialized: false, countryData:{}}
   }
 
 // API call from source and save it in the state.
   componentDidMount() {
     fetch("https://api.covid19api.com/summary")
     .then(response => response.json())
-    .then(final => this.setState({data:final, initialized : true}))
+    .then(data=> {
+      const Countries = data.Countries
+
+      for (let country in Countries)
+        console.log(Countries[country].Country)
+        //TODO add the data to table
+        //TODO pass the table to map and table
+        //TODO Set function to properly sort the data and show the data accordingly
+
+    })
   }
 
 // Every function is called here. Map-container created as a container for the map to be displayed. Everything else is just calls.
   render(){
     return (
       <div>
-        <div id = "map-container" style = {{height: 1200, width : 1200}}></div>
-        <Map/>
         <Table/>
       </div>
     );}
